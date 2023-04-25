20dc1e80be1670493a6b8414f0b5be005b7d6109
luccasissao <87354096+luccasissao@users.noreply.github.com>
2023-04-25 19:54:23 -0300

    Comments and header added

--- Aula_5/Core/Inc/communication.h
+++ Aula_5/Core/Inc/communication.h
@@ -1,12 +1,16 @@
 // ********************************************** //
-// File name:                     buttonsEvents.h //
-// File description:    This file is the header   //
-//                    of buttons events function  //
-//                                                //
+// File name:                     communication.h //
+// File description:   This file is the header of //
+//                   the file that implements the //
+//                   functions and callbacks used //
+//             	        to make the communication //
+//                   between the computer and the //
+//                 system, receiving a number and //
+//                      transmitting its inverse. //
 //                                                //
 // Author names: Gabriel Haj and Luccas Yonei     //
-// Creation date: 13/04/2023                      //
-// Revision date: 19/04/2023					  //
+// Creation date: 24/04/2023                      //
+// Revision date: 25/04/2023		          //
 // ********************************************** //
 
 #ifndef COMMUNICATION_H
@@ -17,11 +21,17 @@
 #include <stdio.h>
 #include "main.h"
 
+/* This struct is used to check and manage the queue */
 typedef struct {
 	unsigned int uiBusyOrder;
 	uint8_t * pAdress;
 	int iSize;
 } xBusyLine;
+
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef * huart);
+
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef * huart);
+
 void vFtoa(char* cStr, double fNum, int iNumCasaDec);
+
 #endif
